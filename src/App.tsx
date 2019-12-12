c96b5793c59abfbc17007865fb1b8cace01db15e
Robert Yuen <dungeonry@hotmail.com>
2019-12-12 06:31:28 -0800

    Loading Indicator added.

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,30 @@ import { Route, Switch, HashRouter } from "react-router-dom";
 import { hot } from "react-hot-loader";
 import Nav from "./Nav";
 import styled from "styled-components";
-import NotFound from "./NotFound";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
+import Spinner from "./shared/Spinner";
 
 const Root = styled.main`
   width: 100%;
 `;
+
+const Projects = React.lazy(() => import("./pages/Projects"));
+const About = React.lazy(() => import("./pages/About"));
+const Home = React.lazy(() => import("./pages/Home"));
+const NotFound = React.lazy(() => import("./NotFound"));
 
 const App: React.FunctionComponent<{}> = () => {
   return (
     <HashRouter>
       <Root>
         <Nav />
-        <Switch>
-          <Route exact path="/projects" component={Projects} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/" component={Home} />
-          <Route component={NotFound} />
-        </Switch>
+        <React.Suspense fallback={<Spinner />}>
+          <Switch>
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/" component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </React.Suspense>
       </Root>
     </HashRouter>
   );
