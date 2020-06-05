a0a01e0793a7a9d73d8850a5605316a0ab0c0c16
robert <dungeonry@hotmail.com>
2020-06-05 07:01:14 -0700

    added keys to components.

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -5,6 +5,7 @@ import ContentFrame from "../shared/ContentFrame";
 import Banner from "../shared/Banner";
 import Summary from "../shared/Summary";
 import Grid from "../shared/Grid";
+import { v4 } from "uuid";
 
 const UnsortedList = styled.ul`
   margin: 0;
@@ -23,7 +24,7 @@ const Skills = () => {
           <h1>Skills / Technologies I use.</h1>
           <UnsortedList>
             {data.skills.map(item => {
-              return <ListItem>{item}</ListItem>;
+              return <ListItem key={v4()}>{item}</ListItem>;
             })}
           </UnsortedList>
         </Summary>
@@ -31,7 +32,7 @@ const Skills = () => {
           <h1>No longer used.</h1>
           <UnsortedList>
             {data.disuse.map(item => {
-              return <ListItem>{item}</ListItem>;
+              return <ListItem key={v4()}>{item}</ListItem>;
             })}
           </UnsortedList>
         </Summary>
